1b1ff6bc351aa045b6cead0e8dfec638e031a8dd
ay65535 <ay65535@icloud.com>
2018-08-20 20:29:51 +0900

    Update

--- configure-home.ps1
+++ configure-home.ps1
@@ -3,18 +3,14 @@
 $env:GITLAB_CPUS = 1
 $env:GITLAB_PORT = 443
 $env:GITLAB_MEMORY = 1526
-#$env:GITLAB_MEMORY = 2048
-#$env:GITLAB_MEMORY = 3072
 $env:GITLAB_SWAP = 3
-#$env:GITLAB_HOST = '127.0.0.1'  # bad
-#$env:GITLAB_HOST = '192.168.0.2'
-#$env:GITLAB_HOST = 'localhost'
 $env:GITLAB_HOSTNAME = $env:GITLAB_HOST
 $env:EXTERNAL_URL = $env:GITLAB_HOSTNAME
+$env:APT_MIRROR = 'http://ftp.jaist.ac.jp/pub/Linux/ubuntu/'
 #$env:VAGRANT_DETECTED_OS = 'mingw'
 
-Get-ChildItem env:\ | Where-Object {$_.Key -imatch "proxy|EXTERNAL_URL|^(VAGRANT|GITLAB|MINGW|MSYS|CYG)"}
+Get-ChildItem env:\ | Where-Object {$_.Key -imatch "proxy|EXTERNAL_URL|^(VAGRANT|GITLAB|MINGW|MSYS|CYG|SMB_USER|APT)"}
 
-Invoke-WebRequest 'https://gitlab.com/gitlab-org/omnibus-gitlab/raw/master/files/gitlab-config-template/gitlab.rb.template' -OutFile 'gitlab.rb.template'
-Invoke-WebRequest 'https://gitlab.com/gitlab-org/gitlab-ce/raw/master/config/gitlab.yml.example' -OutFile 'gitlab.yml.example'
+#Invoke-WebRequest 'https://gitlab.com/gitlab-org/omnibus-gitlab/raw/master/files/gitlab-config-template/gitlab.rb.template' -OutFile 'gitlab.rb.template'
+#Invoke-WebRequest 'https://gitlab.com/gitlab-org/gitlab-ce/raw/master/config/gitlab.yml.example' -OutFile 'gitlab.yml.example'
 #Copy-Item gitlab.rb.template gitlab.rb
--- configure-work.ps1
+++ configure-work.ps1
@@ -1,12 +1,5 @@
 . .\configure-local.ps1
 
-$env:http_proxy = $env:proxy
-$env:https_proxy = $env:proxy
-$env:no_proxy = 'localhost,127.0.0.1,.sock,.local'
-$env:HTTP_PROXY = "${env:http_proxy}"
-$env:HTTPS_PROXY = "${env:https_proxy}"
-$env:NO_PROXY = "${env:no_proxy}"
-
 $env:GITLAB_CPUS = 2
 $env:GITLAB_PORT = 443
 $env:GITLAB_MEMORY = 1749
@@ -14,6 +7,12 @@ $env:GITLAB_SWAP = 4
 $env:GITLAB_HOSTNAME = $env:GITLAB_HOST
 $env:EXTERNAL_URL = $env:GITLAB_HOSTNAME
 $env:APT_MIRROR = 'http://ftp.jaist.ac.jp/pub/Linux/ubuntu/'
+$env:http_proxy = $env:proxy
+$env:https_proxy = $env:proxy
+$env:no_proxy = 'localhost,127.0.0.1,.sock,.local'
+$env:HTTP_PROXY = "${env:http_proxy}"
+$env:HTTPS_PROXY = "${env:https_proxy}"
+$env:NO_PROXY = "${env:no_proxy}"
 #$env:VAGRANT_DETECTED_OS = 'mingw'
 
 Get-ChildItem env:\ | Where-Object {$_.Key -imatch "proxy|EXTERNAL_URL|^(VAGRANT|GITLAB|MINGW|MSYS|CYG|SMB_USER|APT)"}
