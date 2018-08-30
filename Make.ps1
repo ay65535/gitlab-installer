84e37c6a11c6dfd5b66b021e491881b12daad4ff
ay65535 <ay65535@icloud.com>
2018-08-30 12:41:38 +0900

    small fix

--- Make.ps1
+++ Make.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = 'Stop'; Set-PSDebug -Strict -Trace 1
 
 .\Destroy.ps1
 . .\SetEnvs-Work.ps1
+$Pattern = 'proxy|EXTERNAL_URL|^(VAGRANT|GITLAB|MINGW|MSYS|CYG|SMB_USER|APT|EXTERNAL)'
+Get-ChildItem env:\ | Where-Object {$_.Key -imatch "$Pattern"}
+Pause
 vagrant up --no-provision
 vagrant reload --provision-with configure
 vagrant reload --provision-with localize
 vagrant halt
 vagrant snapshot save configured
-$Pattern = 'proxy|EXTERNAL_URL|^(VAGRANT|GITLAB|MINGW|MSYS|CYG|SMB_USER|APT|EXTERNAL)'
-Get-ChildItem env:\ | Where-Object {$_.Key -imatch "$Pattern"}
-Pause
 vagrant reload --provision-with install
 
 $ErrorActionPreference = 'Continue'; Set-PSDebug -Off
