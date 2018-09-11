61ddf1c6b968300863fca5732cb45f170d004977
ay65535 <ay65535@icloud.com>
2018-09-11 22:18:33 +0900

    upd

--- setenvs-home.ps1
+++ setenvs-home.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 Set-PSDebug -Strict -Trace 1
 
-. .\SetEnvs.ps1; $ErrorActionPreference = 'Stop'; Set-PSDebug -Strict -Trace 1
 . .\SetEnvs-Local.ps1; $ErrorActionPreference = 'Stop'; Set-PSDebug -Strict -Trace 1
+. .\SetEnvs.ps1; $ErrorActionPreference = 'Stop'; Set-PSDebug -Strict -Trace 1
 
 $env:GITLAB_CPUS = 2  # 1,2
-$env:GITLAB_MEMORY = 2289  # 1526,2289
+$env:GITLAB_MEMORY = 2048  # 1526,2289
 
 $ErrorActionPreference = 'Continue'; Set-PSDebug -Off
